ca511fb116a3cb92dd82f052cfb422cd3488f709
miaoqs <quansheng.miao@fansunion.com>
2019-07-23 10:54:59 +0800

    解决测试类的====范性测试

--- src/main/java/com/test/demo/web/DemoController.java
+++ src/main/java/com/test/demo/web/DemoController.java
@@ -1,4 +1,4 @@
-package com.test.demo.controller;
+package com.test.demo.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
